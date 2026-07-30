deeb79e39afb0c1dead921c3e4f4a5541ef203fe
mahmoudnabil133 <hodanabil144@gmail.com>
2026-07-30 16:07:44 +0300

    feat(api): register QuizzesModule in AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -8,6 +8,7 @@ import { DocumentsModule } from './modules/documents/documents.module';
 import { EnrollmentsModule } from './modules/enrollments/enrollments.module';
 import { HealthModule } from './modules/health/health.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
+import { QuizzesModule } from './modules/quizzes/quiz.module';
 import { SessionsModule } from './modules/sessions/sessions.module';
 import { StudentModule } from './modules/student/student.module';
 import { TeacherModule } from './modules/teacher/teacher.module';
@@ -46,8 +47,9 @@ import { UsersModule } from './modules/users/users.module';
     TeacherModule,
     DocumentsModule,
     NotificationsModule,
+    QuizzesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
