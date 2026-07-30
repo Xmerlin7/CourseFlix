0fbb42006e16741f307bdd2fbd36310e6399e05d
Mohamed Elgendy <moellgendy@gmail.com>
2026-07-30 03:09:16 +0300

    feat(worker): register ingestion queue in WorkerModule

--- apps/worker/src/worker.module.ts
+++ apps/worker/src/worker.module.ts
@@ -3,21 +3,25 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 import { BullModule } from '@nestjs/bullmq'
 
 @Module({
-imports: [
-  ConfigModule.forRoot({
-    isGlobal: true,
-  }),
-
-  BullModule.forRootAsync({
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: (config: ConfigService) => ({
-      connection: {
-        host: config.get<string>('REDIS_HOST'),
-        port: config.get<number>('REDIS_PORT'),
-      },
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
-  }),
-]
+
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        connection: {
+          host: config.get<string>('REDIS_HOST'),
+          port: config.get<number>('REDIS_PORT'),
+        },
+      }),
+    }),
+
+    BullModule.registerQueue({
+      name: 'ingestion',
+    }),
+  ],
 })
 export class WorkerModule {}
